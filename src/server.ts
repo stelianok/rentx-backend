7bdd78b4189b991cb0a28189953d175485e56128
Kauã Steliano <stelianok@gmail.com>
2021-08-22 10:55:01 -0300

    Implemented new routing system.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.json({ Message: 'Hello World' });
-})
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
+
 app.listen(3333, () => {
   console.log("Let's GOOOOOOO 💜");
 });
