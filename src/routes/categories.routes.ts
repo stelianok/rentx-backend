c7fc1dcb4cf7622020aee5f0e69142445733627b
Kauã Steliano <stelianok@gmail.com>
2021-08-22 12:22:54 -0300

    Implemented services on routes.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
+import { ListCategoriesService } from '../services/ListCategoriesService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -7,13 +9,18 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
+
+  const listCategoriesService = new ListCategoriesService(categoriesRepository);
+
+  const all = listCategoriesService.execute();
 
   return response.json(all);
 })
