ab59512919dc9932022f94b825a89d593961c933
Kauã Steliano <stelianok@gmail.com>
2021-08-22 10:54:47 -0300

    Changed the way routes are organized.

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const category = new Category();
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+export { categoriesRoutes };
