8f6ee11fdeee887a6dc5397275a4a83f6fe258f3
Kauã Steliano <stelianok@gmail.com>
2022-02-02 16:07:59 -0300

    now temporary files are properly deleted

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -8,9 +8,9 @@ interface IImportCategory {
 }
 
 class ImportCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository){}
-  
-  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
+  constructor(private categoriesRepository: ICategoriesRepository) { }
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
     return new Promise((resolve, reject) => {
       const stream = fs.createReadStream(file.path);
       const categories: IImportCategory[] = [];
@@ -20,34 +20,35 @@ class ImportCategoryUseCase {
       stream.pipe(parseFile);
 
       parseFile
-      .on("data", async(line) => {
-        const [name, description] = line;
-        categories.push({
-          name,
-          description,
-        });
-      })
-      .on("end", () => {
-        resolve(categories);
-      })
-      .on("error", (err) => {
-        reject(err);
-      })
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        })
 
-    }); 
+    });
   }
 
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
-    
+
     categories.map(category => {
-      const {name, description} = category;
+      const { name, description } = category;
 
       const existCategory = this.categoriesRepository.findByName(name);
 
-      if(!existCategory){
+      if (!existCategory) {
         this.categoriesRepository.create({
-          name, 
+          name,
           description
         });
       }
