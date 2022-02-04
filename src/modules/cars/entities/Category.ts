8b238630bfce1d9c832a674a650a67464523c8bc
Kauã Steliano <stelianok@gmail.com>
2022-02-04 15:56:27 -0300

    Fixed a mispelling in Category model

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidV4 } from "uuid";
 import { Column, CreateDateColumn, Entity, PrimaryColumn }  from "typeorm"
 
-@Entity("Categories")
+@Entity("categories")
 class Category {
   
   @PrimaryColumn()
