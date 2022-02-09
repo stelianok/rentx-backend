1d94799a9147d6f71370feccff19edaa90c097df
Kauã Steliano <stelianok@gmail.com>
2022-02-09 15:45:46 -0300

    Implemented Tsyringe

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,3 +1,4 @@
+import { inject, injectable } from "tsyringe";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
@@ -6,9 +7,12 @@ interface IRequest {
 
 }
 
+@injectable()
 class CreateCategoryUseCase {
-
-  constructor(private categoriesRepository: ICategoriesRepository) { }
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) { }
 
   async execute({ name, description }: IRequest): Promise<void> {
     const categoryAlreadyExists = await this.categoriesRepository.findByName(name);
---
+++ src/shared/container/index.ts
@@ -0,0 +1,15 @@
+import { container } from "tsyringe";
+import { CategoriesRepository } from "../../modules/cars/repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { SpecificationsRepository } from "../../modules/cars/repositories/SpecificationsRepository";
+
+container.registerSingleton<ICategoriesRepository>(
+  "CategoriesRepository",
+  CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationsRepository>(
+  "SpecificationsRepository",
+  SpecificationsRepository
+);
