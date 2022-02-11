8975075a84c52d9f6eb8554e643f1c01a52edcb7
Kauã Steliano <stelianok@gmail.com>
2022-02-11 17:36:24 -0300

    Can't register two users with the same email, password is now cryptographed

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,6 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcryptjs";
+
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -17,10 +19,18 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if(userAlreadyExists){
+      throw new Error("User already exists");
+    }
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
