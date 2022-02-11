dc6bb9091b3b159ceea065462b62fe7b59bb5e39
Kauã Steliano <stelianok@gmail.com>
2022-02-10 21:41:50 -0300

    Created user controller and useCase

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,11 @@
 import { container } from "tsyringe";
+
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+
 import { CategoriesRepository } from "../../modules/cars/repositories/CategoriesRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
+
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/SpecificationsRepository";
 
@@ -12,4 +17,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 );
