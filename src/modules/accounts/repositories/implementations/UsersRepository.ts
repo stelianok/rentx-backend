35954f0ac425524c23d56f9e66300fa3f4cf221a
Kauã Steliano <stelianok@gmail.com>
2022-02-24 04:49:02 -0300

    Created find by id function on users

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -29,9 +29,15 @@ class UsersRepository implements IUsersRepository {
   }
 
   async findByEmail(email: string): Promise<User> {
-      const user = await this.repository.findOne({ email });
+    const user = await this.repository.findOne({ email });
 
-      return user;
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+
+    return user;
   }
 }
 
