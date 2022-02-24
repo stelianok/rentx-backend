2478649a55802b1c98f3e8ccd6a0c09a162d7849
Kauã Steliano <stelianok@gmail.com>
2022-02-24 04:49:28 -0300

    Created and implemented middleware for authenticating routes.

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request, 
+  response: Response, 
+  next: NextFunction
+  ): Promise<void> {
+
+  const authHeader = request.headers.authorization;
+
+  if(!authHeader){
+    throw new Error("Token missing");
+  }
+
+  const [,token] = authHeader.split(" ");
+
+  try{
+    const { sub: user_id } = verify(
+      token, 
+      "7ba8c2ce25360d28b222b573a210d00e"
+    ) as IPayload;
+    
+    const usersRepository = new UsersRepository();
+    
+    const user = await usersRepository.findById(user_id);
+
+    if(!user){
+      throw new Error("User does not exists!");
+    }
+
+    next();
+  }
+  catch{
+    throw new Error("Invalid token");
+  }
+
+}
