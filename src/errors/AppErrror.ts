bd71aacb9aa948de3085d9cc4ee57d2f8ed9f9cc
Kauã Steliano <stelianok@gmail.com>
2022-02-24 05:02:44 -0300

    Created class for handling errors

---
+++ src/errors/AppErrror.ts
@@ -0,0 +1,9 @@
+export class AppError {
+  public readonly message: string;
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode){
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
