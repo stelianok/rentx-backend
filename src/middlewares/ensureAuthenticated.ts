cd1d6cdc14b935bfb121c61a89de467f0ba3327a
Kauã Steliano <stelianok@gmail.com>
2022-03-25 11:20:34 -0300

    Fixed import

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
-import { AppError } from "../errors/AppErrror";
+import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
 
 interface IPayload {
@@ -8,31 +8,31 @@ interface IPayload {
 }
 
 export async function ensureAuthenticated(
-  request: Request, 
-  response: Response, 
+  request: Request,
+  response: Response,
   next: NextFunction
-  ): Promise<void> {
+): Promise<void> {
 
   const authHeader = request.headers.authorization;
 
-  if(!authHeader){
+  if (!authHeader) {
     throw new AppError("Token missing", 401);
   }
 
-  const [,token] = authHeader.split(" ");
+  const [, token] = authHeader.split(" ");
 
-  try{
+  try {
     const { sub: user_id } = verify(
-      token, 
+      token,
       "7ba8c2ce25360d28b222b573a210d00e"
     ) as IPayload;
-    
+
     const usersRepository = new UsersRepository();
-    
+
     const user = await usersRepository.findById(user_id);
 
-    if(!user){
-      throw new AppError("User does not exists!",401);
+    if (!user) {
+      throw new AppError("User does not exists!", 401);
     }
 
     request.user = {
@@ -41,7 +41,7 @@ export async function ensureAuthenticated(
 
     next();
   }
-  catch{
+  catch {
     throw new AppError("Invalid token", 401);
   }
 
