26aada3bcc0faa13bddcf6491ae4b4c4363f5979
Kauã Steliano <stelianok@gmail.com>
2022-03-27 14:50:00 -0300

    Created routes and Controller for cars

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { authenticateRoutes } from "./authenticate.routes";
+import { carsRoutes } from "./cars.routes";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
@@ -10,6 +11,8 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
+router.use("/cars", carsRoutes);
+
 router.use(authenticateRoutes);
 
 export { router }; 
