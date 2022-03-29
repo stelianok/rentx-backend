ca82690dfda6522f4b9a98640cb84c6e5e12eddf
Kauã Steliano <stelianok@gmail.com>
2022-03-29 09:38:10 -0300

    Added a new method to cars Repository

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -12,6 +12,7 @@ class CarsRepository implements ICarsRepository {
     this.repository = getRepository(Car);
   }
 
+
   async create({
     name,
     description,
@@ -42,6 +43,28 @@ class CarsRepository implements ICarsRepository {
     });
 
     return car;
+  }
+
+  async findAllAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]> {
+    const carsQuery = this.repository
+      .createQueryBuilder("c")
+      .where("available = :available", { available: true });
+
+    if (brand) {
+      carsQuery.andWhere("c.brand = :brand", { brand });
+    }
+
+    if (name) {
+      carsQuery.andWhere("c.name = :name", { name });
+    }
+
+    if (category_id) {
+      carsQuery.andWhere("c.category_id = :category_id", { category_id });
+    }
+
+    const cars = await carsQuery.getMany();
+
+    return cars;
   }
 
 }
--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -4,4 +4,9 @@ import { Car } from "../infra/typeorm/entities/Car";
 export default interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
   findByLicensePlate(license_plate: string): Promise<Car>;
+  findAllAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string,
+  ): Promise<Car[]>;
 }
